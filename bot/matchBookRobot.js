350f7ec92af3ddd0ecd6b50ad62541dcdf7c2dfc
nanichang <nanipaul68@gmail.com>
2018-03-20 18:06:30 +0100

    select trimmed to remove line breaks

--- bot/matchBookRobot.js
+++ bot/matchBookRobot.js
@@ -12,7 +12,7 @@ const puppeteer = require('puppeteer');
 
 // module variables
 const
-	EVENT_URL = 'https://matchbook.com/events/horse-racing/ante-post/doncaster/754191075410013/lincoln-handicap-ante-post',
+	EVENT_URL = 'https://matchbook.com/events/horse-racing/uk/wetherby/755053722660014/live-betting/17-15-wetherby',
 	SELECTIONS_CONTAINER_SELECTOR = 'div.mb-market__runners',
 	MATCHED_AMOUNT_SELECTOR = '.mb-event-header__volume > span:nth-child(2)';
 
@@ -34,68 +34,72 @@ async function bot() {
 		timeout: 180000
 	});
 	await page.waitFor(30 * 1000);
-	// ensure race container selector available
-	if(!!SELECTIONS_CONTAINER_SELECTOR) {
 
-		await page.waitForSelector(SELECTIONS_CONTAINER_SELECTOR, {
-			timeout: 180000
-		});
+	// define selector for validation
+	const raceSelector = await page.waitForSelector(SELECTIONS_CONTAINER_SELECTOR, {
+		timeout: 180000
+	}).catch((error) => {
+		console.log('Race selector not found', error);
+		process.exit(1);
+	});
+	// ensure race container selector available
+	if (!!raceSelector) {
 		// allow 'page' instance to output any calls to browser log to process obj
 		page.on('console', data => console.log(data.text()));
 		// bind to races container and listen for updates to , bets etc
 		await page.$eval(SELECTIONS_CONTAINER_SELECTOR,
-		(target, MATCHED_AMOUNT_SELECTOR) => {
-			// listen for raceStart
-			const observer = new MutationObserver((mutations) => {
-				mutations.forEach(function (ed) {
-					const e = {
-						mutation: ed,
-						el: ed.target,
-						value: ed.target.textContent,
-						oldValue: ed.oldValue
-					};
-					const SELECTION = e.el.parentElement.children[0].innerText;
-					if (e.el.children.length == 2) {
-						num = e.el.className.match(/\d/);
-						letter = e.el.className.match(/(back|lay)/)[0][0];
-						betType = letter + num;
-						odds = e.el.children[0].textContent;
-						liquidity = e.el.children[1].textContent;
-					} else if (e.el.children.length == 3) {
-						num = e.el.className.match(/\d/);
-						letter = e.el.className.match(/(back|lay)/)[0][0];
-						betType = letter + num;
-						odds = e.el.children[1].textContent;
-						liquidity = e.el.children[2].textContent;
-					}
-					if (!!betType && !!odds && !!liquidity && !!SELECTION) {
-						let timestamp = new Date();
-						timestamp = timestamp.toISOString();
-						let matchedAmount = document.querySelector(MATCHED_AMOUNT_SELECTOR).innerText;
-						matchedAmount = Number(matchedAmount.replace(/\D/g, ''));
-						const data = {
-							betType,
-							matchedAmount,
-							timestamp,
-							odds: Number(odds),
-							liquidity: Number(liquidity.slice(1)),
-							selection: SELECTION
+			(target, MATCHED_AMOUNT_SELECTOR) => {
+				// listen for raceStart
+				const observer = new MutationObserver((mutations) => {
+					mutations.forEach(function (ed) {
+						const e = {
+							mutation: ed,
+							el: ed.target,
+							value: ed.target.textContent,
+							oldValue: ed.oldValue
 						};
-						const output = JSON.stringify(data);
-						console.log(output);
-					} 
+						const SELECTION = e.el.parentElement.children[0].innerText;
+						if (e.el.children.length == 2) {
+							num = e.el.className.match(/\d/);
+							letter = e.el.className.match(/(back|lay)/)[0][0];
+							betType = letter + num;
+							odds = e.el.children[0].textContent;
+							liquidity = e.el.children[1].textContent;
+						} else if (e.el.children.length == 3) {
+							num = e.el.className.match(/\d/);
+							letter = e.el.className.match(/(back|lay)/)[0][0];
+							betType = letter + num;
+							odds = e.el.children[1].textContent;
+							liquidity = e.el.children[2].textContent;
+						}
+						if (!!betType && !!odds && !!liquidity && !!SELECTION) {
+							let timestamp = new Date();
+							timestamp = timestamp.toISOString();
+							let matchedAmount = document.querySelector(MATCHED_AMOUNT_SELECTOR).innerText;
+							matchedAmount = Number(matchedAmount.replace(/\D/g, ''));
+							const data = {
+								betType,
+								matchedAmount,
+								timestamp,
+								odds: Number(odds),
+								liquidity: Number(liquidity.slice(1)),
+								selection: SELECTION.replace(/^\n|\n$/g, '')
+							};
+							const output = JSON.stringify(data);
+							console.log(output);
+						}
+					});
+				})
+				observer.observe(target, {
+					attributes: true,
+					childList: true,
+					characterData: true,
+					characterDataOldValue: true,
+					subtree: true
 				});
-			})
-			observer.observe(target, {
-				attributes: true,
-				childList: true,
-				characterData: true,
-				characterDataOldValue: true,
-				subtree: true
-			});
-		}, MATCHED_AMOUNT_SELECTOR);	
-	
-	}else {
+			}, MATCHED_AMOUNT_SELECTOR);
+
+	} else {
 		console.log('ops.. Race selector not found');
 	}
 }
